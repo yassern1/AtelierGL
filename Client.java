09e476ea459b098da5e02bb62962ec1bca8f7875
yassern1 <yassern1@gmail.com>
2016-11-25 23:06:15 +0000

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,12 @@
+public class Client{
+  private String id_clt;
+  private String nom_clt;
+  private String prenom_clt;
+  public Client(){
+  }
+  public Client(String id, String nom, String prenom){
+    this.id_clt=id;
+    this.nom_clt=nom;
+    this.prenom_clt=prenom;
+  }
+}
