9c4bfec738cc62e53f1cffa06f5503a5460041d8
Toshiba <Toshiba@Toshiba-PC>
2016-11-26 12:41:07 +0000

    First Commit

---
+++ Companie.java
@@ -0,0 +1,14 @@
+public class Companie {
+	
+	private String id_comp;
+	private String n_comp;
+	public Companie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Companie(String id, String nom){
+		this.id_comp_=id;
+		this.n_comp=nom;
+	}
+	
+}
